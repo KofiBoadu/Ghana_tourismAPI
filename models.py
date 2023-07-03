ebb4a0f325bf9e52dd598d7d89df8ee2a520d7c0
Daniel Kofi Boadu <mrboadu3@gmail.com>
2023-07-03 04:40:59 -0400

    added a defined a model class with a convert to dict function to convert our request data from our post to be stored in the database

--- models.py
+++ models.py
@@ -8,4 +8,24 @@ class Attraction(db.Model):
     latitude = db.Column(db.Float)
     longitude = db.Column(db.Float)
     description = db.Column(db.Text, nullable=False)
-    opening_hours = db.Column(db.String(
+    opening_hours = db.Column(db.String(50))
+    entry_fees = db.Column(db.Float)
+    website = db.Column(db.String(200))
+    contact_info = db.Column(db.String(200))
+    
+
+
+    def convert_to_dict(self):
+
+        return {
+            "id":self.id,
+            "name": self.name,
+            "location":self.location,
+            "latitude":self.latitude,
+            "longitude":self.longitude,
+            "description":self.description,
+            "opening_hours":self.opening_hours,
+            "entry_fees": self.entry_fees,
+            "website": self.website,
+            "contact_info": self.contact_info
+        }
